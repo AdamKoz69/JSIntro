8146011347e8df593c62a120f79b1b6f4ff983ca
AdamKoz69 <119588818+AdamKoz69@users.noreply.github.com>
2023-01-05 11:54:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function Button(){
+    const text = document.getElementById("out");
+
+    text.innerHTML = "I am a random color"; 
+
+    var r = Math.floor(Math.random()*255); 
+    var g = Math.floor(Math.random()*255); 
+    var b = Math.floor(Math.random()*255); 
+    text.style.color = "RGB(" + r + "," + g + "," + b + ")"; 
+
+    window.alert("The color has been changed to: " + text.style.color) 
+}
+
+function textChange(){
+    const textbox = document.getElementById("Text").value;
+    const count = document.getElementById("count");
+
+    if(textbox.length > 255){
+        window.alert("Text exceeding 255! You put in " + textbox.length + " words!");
+        var slice = textbox.slice(0,255);
+        document.getElementById("Text").value = slice;
+        count.innerHTML = "255/255";
+    } else {
+        count.innerHTML = textbox.length + "/255";
+    }   
+}
+
+function greet(){
+    const textarea = document.getElementById("fn").value;
+    const greet = document.getElementById("greet");
+
+    if(!textarea.match(/\S/)){
+        window.alert("Please enter a name!");
+    } else {
+        var name = document.getElementById("fn");
+        var FLetter = name.value.slice(0,1);
+        var LLetters = name.value.slice(1,name.value.length);
+        var construct = FLetter.toUpperCase() + LLetters;
+        greet.innerHTML = "Hello " + construct + ", How are you today?";
+    }
+}
+
+function LOVE(){
+    const p1 = document.getElementById("p1").value
+    const p2 = document.getElementById("p2").value
+    const out = document.getElementById("loveout")
+
+    if(!p1.match(/\S/) || !p2.match(/\S/)){
+        window.alert("Person 1 or/and Person 2 Name is Empty");
+    } else {
+        let rand = Math.floor(Math.random()*100);
+        out.innerHTML = "the love percentage for " + p1 + " and " + p2 + " is at " + rand +"%";
+    }
+}
